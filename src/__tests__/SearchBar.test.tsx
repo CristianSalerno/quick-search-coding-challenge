e6ae63799ca5610ed38893244da31906a610e037
cristian salerno <csalernodev@icloud..com>
2025-04-09 20:26:08 +0200

    refactor: test suite refactor

--- src/__tests__/SearchBar.test.tsx
+++ src/__tests__/SearchBar.test.tsx
@@ -2,37 +2,47 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { SearchBar } from '../components/SearchBar';
 import { store } from '../app/store';
-import { expect, test } from 'vitest';
-
-test('SearchBar updates value and triggers search when input has 2 or more characters', async () => {
-  render(
-    <Provider store={store}>
-      <SearchBar />
-    </Provider>
-  );
-
-  const input = screen.getByPlaceholderText(/Type at least 2 characters/i);
+import { describe, expect, test, beforeEach } from 'vitest';
+import { SearchResults } from '../components/SearchResults';
 
 
-  fireEvent.change(input, { target: { value: 'h' } });
-  expect(store.getState().search.query).toBe('h');
-
-  
-  fireEvent.change(input, { target: { value: 'ha' } });
-  await waitFor(() => {
-    expect(store.getState().search.query).toBe('ha');
+describe("Search bar tests", () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <SearchBar />
+      </Provider>
+    );
   });
 
+  test('SearchBar should not update if user types 1 character', async () => {
+    const input = screen.getByPlaceholderText(/Type at least 2 characters/i);
   
-  fireEvent.change(input, { target: { value: 'harry' } });
-  await waitFor(() => {
-    expect(store.getState().search.query).toBe('harry');
-    expect(screen.getByText('Loading...')).toBeTruthy(); 
+    fireEvent.change(input, { target: { value: 'h' } });
+    expect(store.getState().search.query).toBe('h');
+    expect(screen.queryByText('Loading...')).toBeFalsy(); 
+  });
 
+  test('SearchBar should call backend if user types 2 or more characters and show a loading state', async () => {
+    const input = screen.getByPlaceholderText(/Type at least 2 characters/i);
+    fireEvent.change(input, { target: { value: 'ha' } });
+    await waitFor(() => {
+      expect(store.getState().search.query).toBe('ha');
+      expect(screen.queryByText('Loading...')).toBeTruthy(); 
+    });
   })
- 
-  await waitFor(() => {
+
+  test("SearchBar should call backend and display results when user types 2 or more characters", async () => {
+    const input = screen.getByPlaceholderText(/Type at least 2 characters/i);
+    fireEvent.change(input, { target: { value: 'harry' } });
+   
+    await waitFor(() => {
+      expect(screen.queryByText('Loading...')).not.toBeInTheDocument()
+    });
+
     const bookItems = screen.queryAllByTestId(/^book-item-/);
     expect(bookItems.length).toBeGreaterThan(0); 
   });
-});
+  
+})
+
--- src/__tests__/SearchResult.test.tsx
+++ src/__tests__/SearchResult.test.tsx
@@ -2,35 +2,40 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { SearchBar } from '../components/SearchBar';
 import { store } from '../app/store';
-import { expect, test } from 'vitest';
+import { expect, test, describe, beforeEach } from 'vitest';
 import { SearchResults } from '../components/SearchResults';
 
 
-test('Renders search reults', async () => {
-    render(
-        <Provider store={store}>
-            <SearchBar />
-            <SearchResults />
-        </Provider>
-    );
-
-    const input = screen.getByPlaceholderText(/Type at least 2 characters/i);
-    const bookItems = screen.queryAllByTestId(/^book-item-/);
-
-    fireEvent.change(input, { target: { value: 'h' } });
-
-    await waitFor(() => {
-        const bookItems = screen.queryAllByTestId(/^book-item-/);
-        expect(bookItems.length).toBe(0);
+describe('Search results Tests', () => {
+    beforeEach(() => {
+        render(
+            <Provider store={store}>
+                <SearchBar />
+                <SearchResults />
+            </Provider>
+        );
     });
-        
-    await waitFor(() => {
-        const bookItems = screen.queryAllByTestId(/^book-item-/);
-        fireEvent.change(input, { target: { value: 'harry' } });
-        expect(screen.queryByText('Loading...')).toBeFalsy()
-        expect(screen.queryByText('No results found.')).toBeFalsy();
-        expect(screen.queryByText('Error loading results')).toBeFalsy();
-        expect(bookItems.length).toBeGreaterThan(0)
-    })
- 
+
+    test('Should not render results when user types 1 character', async () => {
+        const input = screen.getByPlaceholderText(/Type at least 2 characters/i);
+        fireEvent.change(input, { target: { value: 'h' } });
+
+        await waitFor(() => {
+            const bookItems = screen.queryAllByTestId(/^book-item-/);
+            expect(bookItems.length).toBe(0);
+        });
+    });
+
+    test('Should render results when user types 2 or more characters', async () => {
+        const input = screen.getByPlaceholderText(/Type at least 2 characters/i);
+
+        await waitFor(() => {
+            const bookItems = screen.queryAllByTestId(/^book-item-/);
+            fireEvent.change(input, { target: { value: 'harry' } });
+            expect(screen.queryByText('Loading...')).toBeFalsy()
+            expect(screen.queryByText('No results found.')).toBeFalsy();
+            expect(screen.queryByText('Error loading results')).toBeFalsy();
+            expect(bookItems.length).toBeGreaterThan(0)
+        })
+    });
 });
